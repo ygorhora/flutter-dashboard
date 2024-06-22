af245c00f916d5fe32f7a970691f153e8d15eee1
ygorhora <ygorhora@gmail.com>
2024-06-22 14:55:06 -0300

    create a menu data (based on model)

---
+++ lib/data/side_menu_data.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dashboard/model/menu_model.dart';
+
+class SideMenuData {
+  final menu = const <MenuModel>[
+    MenuModel(icon: Icons.home, title: 'Dashboard'),
+    MenuModel(icon: Icons.person, title: 'Profile'),
+    MenuModel(icon: Icons.run_circle, title: 'Exercises'),
+    MenuModel(icon: Icons.settings, title: 'Settings'),
+    MenuModel(icon: Icons.history, title: 'History'),
+    MenuModel(icon: Icons.logout, title: 'SignOut'),
+  ];
+}
