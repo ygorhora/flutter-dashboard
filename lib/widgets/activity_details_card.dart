bc2a85f0abc2afa4ffb0cb16d2ce5ec7b649372b
ygorhora <ygorhora@gmail.com>
2024-06-22 18:14:47 -0300

    grid health details

---
+++ lib/widgets/activity_details_card.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dashboard/data/health_details.dart';
+import 'package:flutter_dashboard/widgets/custom_card_widget.dart';
+
+class ActivityDetailsCard extends StatelessWidget {
+  const ActivityDetailsCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final healthDetails = HealthDetails();
+    return GridView.builder(
+      itemCount: healthDetails.healthData.length,
+      shrinkWrap: true,
+      physics: const ScrollPhysics(),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 4,
+        crossAxisSpacing: 15,
+        mainAxisSpacing: 15,
+      ),
+      itemBuilder: (context, index) {
+        return CustomCard(
+          child: Container(),
+        );
+      },
+    );
+  }
+}
