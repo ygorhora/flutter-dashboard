a3085207c72a9793da49e0d7fbfb6a59ae43d6dc
ygorhora <ygorhora@gmail.com>
2024-06-22 14:26:44 -0300

    separate scren in 3 parts

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -5,6 +5,29 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Row(
+          children: [
+            Expanded(
+              flex: 2,
+              child: Container(
+                color: Colors.red,
+              ),
+            ),
+            Expanded(
+              flex: 7,
+              child: Container(
+                color: Colors.blue,
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: Container(color: Colors.green),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
